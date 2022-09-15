edf7f5cfb7c6d1757dee32c99b1e522adba20f97
US\CUJ5GA <JoseManuel.CumplidoBernal@mx.bosch.com>
2022-09-15 15:39:18 -0500

    Cambio de name: "name123"

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysite',
+        'NAME': 'Name123',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
